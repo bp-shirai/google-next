bb8a8be03c23effe0e00e1b6135e01a7efbdcb21
bp.shirai <bp.shirai@gmail.com>
2024-07-25 16:54:46 +0900

    create search page for web and image

--- src/app/components/HomeSearch.jsx
+++ src/app/components/HomeSearch.jsx
@@ -4,16 +4,14 @@ import React from "react";
 import { AiOutlineSearch } from "react-icons/ai";
 import { BsFillMicFill } from "react-icons/bs";
 import { useState } from "react";
-import { useRouter } from "next/router";
+import { useRouter } from "next/navigation";
 
 export default function HomeSearch() {
   const [input, setInput] = useState("");
-  //console.log(input);
   const [randomSearchLoading, setRandomSearchLoading] = useState(false);
-
   const router = useRouter();
   const handleSubmit = (e) => {
-    e.preventDefault(); // submitイベントの本来の動作を止める
+    e.preventDefault();
     if (!input.trim()) return;
     router.push(`/search/web?searchTerm=${input}`);
   };
@@ -21,7 +19,7 @@ export default function HomeSearch() {
   const randomSearch = async (e) => {
     setRandomSearchLoading(true);
     const response = await fetch("https://random-word-api.herokuapp.com/word")
-      .then((res) => res.json)
+      .then((res) => res.json())
       .then((data) => data[0]);
     if (!response) return;
     router.push(`/search/web?searchTerm=${response}`);
@@ -42,7 +40,7 @@ export default function HomeSearch() {
         />
         <BsFillMicFill className="text-lg" />
       </form>
-      <div className="flex flex-col space-y-2 sm:space-y-0 justify-center sm:flex-row mt-8">
+      <div className="flex flex-col space-y-2 sm:space-y-0 justify-center sm:flex-row mt-8 sm:space-x-4">
         <button
           className="bg-[#f8f9fa] rounded-md text-sm text-gray-800 hover:ring-gray-200 focus:outline-none active:ring-gray-300 hover:shadow-md w-36 h-10 transition-shadow"
           onClick={handleSubmit}
